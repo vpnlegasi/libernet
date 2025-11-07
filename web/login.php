88d68eef175a0a9c72a50d6eb912c1b55d6d22bd
vpnlegasi <97322084+vpnlegasi@users.noreply.github.com>
2025-11-07 22:28:44 +0800

    Update login.php

--- web/login.php
+++ web/login.php
@@ -32,7 +32,7 @@
             <div class="col-lg-6 col-md-12 mx-auto">
                 <div class="card mb-2">
                     <div class="card-header">
-                        <h3 class="text-center">Libernet</h3>
+                        <h3 class="text-center">Modded By Vpn Legasi</h3>
                     </div>
                     <div class="card-body">
                         <form action="" method="post">
