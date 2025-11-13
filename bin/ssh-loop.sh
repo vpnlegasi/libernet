634d2aab225db79b45f643c003a7d2cfc2839d9d
vpnlegasi <97322084+vpnlegasi@users.noreply.github.com>
2025-11-13 20:13:31 +0800

    Update ssh-loop.sh

--- bin/ssh-loop.sh
+++ bin/ssh-loop.sh
@@ -27,33 +27,9 @@ function connect_with_proxy() {
     "${1}@${3}"
 }
 
-# fungsi semak IP dan bandingkan
-function check_ip() {
-  CURRENT_IP=$(ip -4 addr show | grep -v "127.0.0.1" | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n1)
-  if [[ -z "$CURRENT_IP" ]]; then
-    # tiada IP detected, reconnect
-    "${LIBERNET_DIR}/bin/log.sh" -w "SSH tunnel lost IP, restarting..."
-    return 1
-  fi
-
-  if [[ "$CURRENT_IP" != "$LAST_IP" ]]; then
-    LAST_IP="$CURRENT_IP"
-    "${LIBERNET_DIR}/bin/log.sh" -w "SSH tunnel IP changed, restarting..."
-    return 1
-  fi
-
-  return 0
-}
-
-LAST_IP=""
-
 case "${1}" in
   -d)
     while true; do
-      if ! check_ip; then
-        sleep 3
-        continue
-      fi
       # command username password host port dynamic_port
       connect "${2}" "${3}" "${4}" "${5}" "${6}"
       sleep 3
@@ -61,10 +37,6 @@ case "${1}" in
     ;;
   -e)
     while true; do
-      if ! check_ip; then
-        sleep 3
-        continue
-      fi
       # command username password host port dynamic_port proxy_ip proxy_port
       connect_with_proxy "${2}" "${3}" "${4}" "${5}" "${6}" "${7}" "${8}"
       sleep 3
